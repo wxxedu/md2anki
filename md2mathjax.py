d122e115917b8c957c2c5bac93ff5a40b0925602
Xiuxuan Wang <wxxedu@126.com>
2020-12-29 13:16:47 +0800

    Added Explanation

--- md2mathjax.py
+++ md2mathjax.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# 这个部分的功能在于把Markdown文件中的美元符号转换Anki MathJax中相对应的\(\)和\[\]
+# 具体来说：把行内公式$\dfrac{1}{2}$改成\(\dfrac{1}{2}\)，把display style的行间公式$$\dfrac{1}{2}$$改成\[\dfrac{1}{2}\]
+
 import definitions
 
 def mathjaxAnki(line):
